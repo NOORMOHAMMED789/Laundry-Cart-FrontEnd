378df4688c2a00b3cd179133abf7240e382fff8d
Noor Mohammed <noormohammedknr2@gmail.com>
2022-12-20 01:40:36 +0530

    Footer3 added

--- client/src/Components/Footer3/Footer3.jsx
+++ client/src/Components/Footer3/Footer3.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Footer3.css";
 
 const Footer3 = () => {
-  return <div>Footer3</div>;
+  return (
+    <div className="footer3">
+      <p>2021 &copy Laundry</p>
+      Footer3
+    </div>
+  );
 };
 
 export default Footer3;
--- client/src/SigninComp.jsx
+++ client/src/SigninComp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer1 from "./Components/Footer1/Footer1";
 import Footer2 from "./Components/Footer2/Footer2";
+import Footer3 from "./Components/Footer3/Footer3";
 import SigninHeader from "./Components/Header/SigninHeader";
 import SignupPage from "./Components/SignupPage/SignupPage";
 
@@ -11,6 +12,7 @@ const SigninComp = () => {
       <SignupPage />
       <Footer1 />
       <Footer2 />
+      <Footer3 />
     </div>
   );
 };
