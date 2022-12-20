8d0a90c96cef51461c34d78607b850c01706e796
Noor Mohammed <noormohammedknr2@gmail.com>
2022-12-20 13:15:27 +0530

    Register comp added

--- client/src/RegisterComp.jsx
+++ client/src/RegisterComp.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SigninHeader from "./Components/Header/SigninHeader";
+import RegisterPage from "./Components/RegisterPage/RegisterPage";
 
 const RegisterComp = () => {
-  return <div>Hello from Register Component</div>;
+  return (
+    <div>
+      <SigninHeader />
+      <RegisterPage />
+    </div>
+  );
 };
 
 export default RegisterComp;
