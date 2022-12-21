9784449aaeb2a1d29d23e27d9b0d7ee86aa53a16
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-22 04:20:47 +0530

    connect with api

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./Components/Header/SigninHeader";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Footer from "./Components/Footer3/Footer3";
 import OrderRow from "./Components/OrderRow/OrderRow";
 
+const URL = process.env.REACT_APP_API_KEY || "http://localhost:3001"
+
 export default function Home() {
     const navigate = useNavigate();
-    const [fetchedData, setFetchedData] = useState(null);
+    const [fetchedData, setFetchedData] = useState([]);
+
+    useEffect(() => {
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiemVlc2hhbjQwMDI5MTFAZ21haWwuY29tIiwiaWF0IjoxNjcxNjYwNjIwLCJleHAiOjE2NzE2NjQyMjB9.FEDsioSsHwVgr2rgwwDwrglX_Xet-dp9SQTMDt4yQ8U";
+
+        fetch(URL + '/api/v1/order/', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        }).then((response) => {
+            if (response.status === 403) return navigate("/");
+            return response.json()
+        })
+            .then(data => { setFetchedData(data.orders.reverse()); console.log(data) });
+    }, [])
 
     const handleCreateClick = () => {
         navigate("/catelog");
@@ -19,9 +37,9 @@ export default function Home() {
             < Sidebar />
             <div className="home-container" style={{ height: "85vh", marginLeft: "4vw" }}>
                 <div className="home-container-hero" style={{ height: "10vh" }}>
-                    <div htmlFor="order-count" style={count_style}>Orders | 0</div>
+                    <div htmlFor="order-count" style={count_style}>Orders | {fetchedData?.length}</div>
                     <div style={search_style}>
-                        {fetchedData ? <button className="btn-vt" onClick={() => handleCreateClick()}>Create</button>
+                        {fetchedData.length ? <button className="btn-vt" onClick={() => handleCreateClick()}>Create</button>
                             : <></>}
                         <div style={{ display: "inline-block", borderBottom: '1px solid gray' }}>
                             <img src="./icons/search.svg" alt="search" />
@@ -30,7 +48,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div style={{ margin: "0 5vw" }}>
-                    {fetchedData ?
+                    {fetchedData.length ?
                         < OrderRow data={fetchedData} />
                         :
                         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "60vh", flexDirection: "column" }}>
