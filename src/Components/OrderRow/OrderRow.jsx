52d6ae72327ff58ee19d6d4b4cf2eb13b6c29507
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-22 21:26:00 +0530

    modified

--- src/Components/OrderRow/OrderRow.jsx
+++ src/Components/OrderRow/OrderRow.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { getToken } from "../../authOperations";
 import style from "./OrderRow.module.css";
 
+const URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+
 const OrderRow = ({ data }) => {
+    const navigate = useNavigate();
     data?.map(obj => {
         const date = new Date(obj.createdAt)
         return obj.createdAt = date.toLocaleString();
     });
+
+    const handleCancel = (order_id) => {
+        const token = getToken("token");
+        fetch(`${URL}/api/v1/order/${order_id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: JSON.stringify({ status: "Cancelled" })
+        })
+        .then((response) => {
+            if (response.status === 403 || response.status === 401) return navigate("/");
+                window.location.reload();
+            });
+    }
+
+    const handleView = () => {
+
+    }
+
     return (
         <table id="order-table" className={style.fontSize}>
             <thead>
@@ -32,9 +58,9 @@ const OrderRow = ({ data }) => {
                         <td>{obj.storePhone}</td>
                         <td>{obj.totalItems}</td>
                         <td style={{ color: "#5861AE", fontWeight: "bold" }}>{obj.price}</td>
-                        <td>{obj.status}</td>
-                        <td><button className="btn-vt" style={{ color: "red", border: "none", background: "transparent" }}>Cancel</button> </td>
-                        <td><img src="/icons/view.png" alt="view" width="15vw" /></td>
+                        <td style={obj.status === "Cancelled"?{color: "red", fontWeight: "bold"}:{}}>{obj.status}</td>
+                        <td><button className="btn-vt" style={button_style} onClick={() => handleCancel(obj.orderId)}>Cancel</button> </td>
+                        <td><img src="/icons/view.png" alt="view" width="15vw" onClick={handleView} /></td>
                     </tr>
                 ))
                 }
@@ -44,3 +70,9 @@ const OrderRow = ({ data }) => {
 }
 
 export default OrderRow;
+
+const button_style = {
+    color: "red",
+    border: "none",
+    background: "transparent"
+}
