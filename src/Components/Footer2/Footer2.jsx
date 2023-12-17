33692ad6220af9d47896ddd443df5a9c4456ccaa
Noor Mohammed <noormohammedknr2@gmail.com>
2023-12-17 15:53:51 +0530

    UI is updation

--- src/Components/Footer2/Footer2.jsx
+++ src/Components/Footer2/Footer2.jsx
@@ -6,43 +6,51 @@ const Footer2 = () => {
   return (
     <div className="footer2">
       <section className="footer2_sec1">
-        <h4 className="footer2_title">ABOUT US</h4>
+        <h4 className="footer2_title">Online Laundry cart Services</h4>
         <p className="footer2_para">Doorstep Wash & Dryclean Service</p>
       </section>
       <section className="footer2_sec2">
         <ul className="footer2_first__ul">
-          <Link to={"/home"}><li>Home</li></Link>
-          <Link to={"/"}><li>Sign In</li></Link>
-          <Link to={"/register"}><li>Register</li></Link>
-          <li>Pricing</li>
+          <Link to={"/"}>
+            <li>Sign In</li>
+          </Link>
+          <Link to={"/about"}>
+            <li>About us</li>
+          </Link>
+          <Link to={"/about"}>
+            <li>Pricing</li>
+          </Link>
         </ul>
       </section>
+
       <section className="footer2_sec3">
         <ul className="footer2_first__ul">
           <li>Career</li>
           <li>Blogs</li>
-          <li>Create</li>
           <li>Contact</li>
         </ul>
       </section>
 
       <section className="footer2_sec2">
-        <h4 className="footer2_sec4__title">SOCIAL MEDIA</h4>
+        <h4 className="footer2_sec4__title">LAUNDRY CART</h4>
         <div className="footer2_socialmedia">
           <img
             src="/icons/facebook.svg"
             alt="facebook"
-            style={{ width: "30px" }}
+            width="20px"
+            height="20px"
           />
           <img
             src="/icons/instagram.svg"
             alt="instagram"
-            style={{ width: "30px" }}
+            width="20px"
+            height="20px"
           />
           <img
             src="/icons/linkedin.svg"
             alt="linkedin"
-            style={{ width: "30px" }}
+            width="20px"
+            height="20px"
           />
         </div>
       </section>
